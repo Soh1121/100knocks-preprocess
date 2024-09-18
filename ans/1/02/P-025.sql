8627a7cf2dc6421be2f5f0cda70244aebbd3d22c
Soh1121 <satou.shg@gmail.com>
2024-09-18 09:41:06 +0900

    [add] P-025 を回答

---
+++ ans/1/02/P-025.sql
@@ -0,0 +1,21 @@
+WITH
+    ranked_data
+AS (
+    SELECT
+        *,
+        row_number() OVER (
+            PARTITION BY customer_id
+            ORDER BY sales_ymd ASC
+        ) AS rank
+    FROM
+        receipt
+)
+SELECT
+    customer_id, sales_ymd
+FROM
+    ranked_data
+WHERE
+    rank = 1
+LIMIT
+    10
+;
