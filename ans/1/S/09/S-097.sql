cab769bdb96d8772b7578662a3c8e0c1d4b49494
Soh1121 <satou.shg@gmail.com>
2025-01-16 09:34:22 +0900

    [add] S-097 を追加

---
+++ ans/1/S/09/S-097.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS product_full_import;
+CREATE TABLE product_full_import (
+    product_cd              VARCHAR(10),
+    category_major_cd       VARCHAR(2),
+    category_major_name     VARCHAR(32),
+    category_medium_cd      VARCHAR(4),
+    category_medium_name    VARCHAR(32),
+    category_small_cd       VARCHAR(6),
+    category_small_name     VARCHAR(32),
+    unit_price              INTEGER,
+    unit_cost               INTEGER
+);
+
+COPY
+    product_full_import
+FROM
+    '/tmp/data/S_product_full_UTF-8_header.csv'
+WITH
+    CSV HEADER
+ENCODING
+    'UTF-8'
+;
+
+SELECT
+    *
+FROM
+    product_full_import
+LIMIT
+    3
+;
